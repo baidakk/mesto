3fa098ddfa6fbbc38e2c89d70d21e85302b83f80
baidakk <109903833+baidakk@users.noreply.github.com>
2022-09-28 21:20:16 +0500

    fix: js / buttons / profile

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,40 @@
-const profileEditButton = document.querySelector('.profile__edit-button');
-const popUp = document.querySelector('.popup');
-const popUpClose = popUp.querySelector('.popup__close');
-let formElement = document.querySelector('.popup__form'); 
-let nameInput = formElement.querySelector('#name');  
-let jobInput = formElement.querySelector('#role'); 
+const popUp = document.querySelector(".popup");
+const profileEditButton = document.querySelector(".profile__edit-button");
+const popUpClose = popUp.querySelector(".popup__close");
+let formElement = document.querySelector(".popup__form");
+let nameInput = formElement.querySelector("#name");
+let jobInput = formElement.querySelector("#role");
 
-const popUpToggle = function(){
-    popUp.classList.toggle('popup_opened');
-    document.querySelector('.popup__form').querySelector('#name').value = '';
-    document.querySelector('.popup__form').querySelector('#role').value = '';
-};
-
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    let nameValue = nameInput.value;
-    let jobValue = jobInput.value;
-    let profileHeading = document.querySelector('.profile__heading-name');
-    let profileRole = document.querySelector('.profile__description');
-    profileHeading.textContent = nameValue;
-    profileRole.textContent = jobValue;
-    popUpToggle()
+function openPopup() {
+  nameInput.value = document.querySelector(
+    ".profile__heading-name"
+  ).textContent;
+  jobInput.value = document.querySelector(".profile__description").textContent;
+  popUp.classList.toggle("popup_opened");
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
-profileEditButton.addEventListener('click', popUpToggle);
-popUpClose.addEventListener('click', popUpToggle);
+function closePopup() {
+  popUp.classList.remove("popup_opened");
+}
 
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  let nameValue = nameInput.value;
+  let jobValue = jobInput.value;
+  let profileHeading = document.querySelector(".profile__heading-name");
+  let profileRole = document.querySelector(".profile__description");
+  profileHeading.textContent = nameValue;
+  profileRole.textContent = jobValue;
+  closePopup();
+}
+
+formElement.addEventListener("submit", formSubmitHandler);
+profileEditButton.addEventListener("click", openPopup);
+popUpClose.addEventListener("click", closePopup);
+
+// Оставлю переключение лайков на будущий спринт :)
 
 // const likeButton = document.querySelector('.cards__like-button');
-
 // const likeToggle = function(){
 //     likeButton.classList.toggle('cards__like-button_deactive');
 // };
